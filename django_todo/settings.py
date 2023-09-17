6b353ea733668240c54f6d884474fc0d5e10cd78
Ila-bura <>
2023-09-17 16:05:56 +0000

    Add variable

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -27,10 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    'django-insecure-s4vr))oj6-l(eo#zrtyvt6qwsy*61-_jws68txl'
-    ')b_omnep_gh'
-)
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
